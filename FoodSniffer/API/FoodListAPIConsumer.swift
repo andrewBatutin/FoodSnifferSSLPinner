b1f204f5091f21e7eadd509dfd831ec79654f31d
Andrew Batutin <andriy.batutin@email.com>
2018-08-01 15:56:00 +0300

    added cert pinning

--- FoodSniffer/API/FoodListAPIConsumer.swift
+++ FoodSniffer/API/FoodListAPIConsumer.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Security
 
 enum DaySegments:String,Codable{
     
@@ -26,6 +27,11 @@ struct FoodItem:Codable {
 @objc
 final class FoodListAPIConsumer : NSObject, URLSessionDelegate{
     
+    let certificates: [Data] = {
+        let url = Bundle.main.url(forResource: "dropboxcom", withExtension: "crt")!
+        let data = try! Data(contentsOf: url)
+        return [data]
+    }()
     
     let foodListURL = "https://www.dropbox.com/s/8ipgua5mfiakhxy/MockFoodListJSON.json?dl=1"
     
@@ -33,7 +39,7 @@ final class FoodListAPIConsumer : NSObject, URLSessionDelegate{
     func loadFoodList(_ callback: @escaping ( [FoodItem]? ) -> ()){
         
         guard let foodUrl = URL(string: foodListURL) else { return }
-        let session = URLSession(configuration: .default)
+        let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
         let dataTask = session.dataTask(with: foodUrl) { (data, response, error) in
             
             if let networkError = error {
@@ -71,3 +77,22 @@ final class FoodListAPIConsumer : NSObject, URLSessionDelegate{
     }
     
 }
+
+
+extension FoodListAPIConsumer {
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        if let trust = challenge.protectionSpace.serverTrust, SecTrustGetCertificateCount(trust) > 0 {
+            
+            if let certificate = SecTrustGetCertificateAtIndex(trust, 0) {
+                let data = SecCertificateCopyData(certificate) as Data
+                if certificates.contains(data) {
+                    completionHandler(.useCredential, URLCredential(trust: trust))
+                    return
+                }
+            }
+        }
+        completionHandler(.rejectProtectionSpace, nil)
+    }
+}
+
